f71be7101707b0eee4615342b93a0ee42c83da14
seth@minibuntu <seth.quantix@gmail.com>
2023-04-05 20:55:43 +0200

    not elegant, but it works

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -144,31 +144,37 @@ fn main() {
     let mut acc = 0.0;
     let mut cycles = 0;
     let mut clock = Clock::new(4);
+    let mut run = false;
     app.create(menu)
         .run(move |app| {
             if app.emu.is_running() {
-                if st.paused() {
+                if st.paused() { st.start(); }
+                if run {
+                    acc += current.elapsed().as_secs_f64();
                     current = std::time::Instant::now();
-                    st.start();
+                    let cy = app.emu.cycle_time();
+                    while acc >= cy {
+                        cycles += 1;
+                        app.emu.cycle(clock.tick());
+                        acc -= cy;
+                    }
                 }
-                acc += current.elapsed().as_secs_f64();
-                current = std::time::Instant::now();
-                let cy = app.emu.cycle_time();
-                while acc >= cy {
-                    cycles += 1;
-                    app.emu.cycle(clock.tick());
-                    acc -= cy;
+                if st.elapsed().as_secs() != 0 {
+                    if !run {
+                        run = true;
+                        current = std::time::Instant::now();
+                    }
+                    let t = cycles as f64 / st.elapsed().as_secs_f64();
+                    let p = (t / 4194304.) * 100.;
+                    log::debug!("cycles: {:.0} ({:0.2} %)", t, p);
+                    st.stop();
+                    st.start();
+                    cycles = 0;
                 }
             } else {
-                st.pause();
-            }
-            if st.elapsed().as_secs() != 0 {
-                let t = cycles as f64 / st.elapsed().as_secs_f64();
-                let p = (t / 4194304.) * 100.;
-                log::debug!("cycles: {:.0} ({:0.2} %)", t, p);
                 st.stop();
-                st.start();
                 cycles = 0;
+                run = false;
             }
         });
 }
