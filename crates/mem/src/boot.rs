c7b14054db5704f508a2d18785f0c737df1fddd0
seth@minibuntu <seth.quantix@gmail.com>
2023-05-11 15:57:50 +0200

    boot

--- crates/mem/src/boot.rs
+++ crates/mem/src/boot.rs
@@ -1,9 +1,11 @@
 use shared::mem::Mem;
 use shared::rom::Rom;
-use crate::mbc::{Mbc, MemoryController};
+use shared::serde::{Deserialize, Serialize};
+
+use crate::mbc::{Mbc, MemoryController, Unplugged};
 
 struct DmgBoot {
-    raw: Vec<u8>
+    raw: Vec<u8>,
 }
 
 impl Mem for DmgBoot {
@@ -22,13 +24,9 @@ impl BootSection for DmgBoot {
     fn contains(&self, addr: u16) -> bool { (0..0x100).contains(&addr) }
 }
 
+#[derive(Serialize, Deserialize)]
 struct CgbBoot {
-    raw: Vec<u8>
-}
-
-impl BootSection for () {
-    fn new() -> Self where Self: Sized { () }
-    fn contains(&self, _addr: u16) -> bool { false }
+    raw: Vec<u8>,
 }
 
 impl BootSection for CgbBoot {
@@ -55,17 +53,18 @@ trait BootSection: Mem {
     fn contains(&self, addr: u16) -> bool;
 }
 
-pub(crate) struct Boot<MBC: Mbc> {
+pub(crate) struct Boot {
     boot: Box<dyn BootSection>,
-    inner: Option<MBC>
+    inner: Box<dyn Mbc>,
 }
 
-impl<MBC: Mbc> Mem for Boot<MBC> {
+
+impl Mem for Boot {
     fn read(&self, addr: u16, absolute: u16) -> u8 {
         if self.boot.contains(absolute) {
             self.boot.read(addr, absolute)
         } else {
-            self.inner.as_ref().unwrap().read(addr, absolute)
+            self.inner.read(addr, absolute)
         }
     }
 
@@ -73,35 +72,35 @@ impl<MBC: Mbc> Mem for Boot<MBC> {
         if self.boot.contains(absolute) {
             self.boot.value(addr, absolute)
         } else {
-            self.inner.as_ref().unwrap().value(addr, absolute)
+            self.inner.value(addr, absolute)
         }
     }
 
     fn get_range(&self, st: u16, len: u16) -> Vec<u8> {
-        self.inner.as_ref().unwrap().get_range(st, len)
+        self.inner.get_range(st, len)
     }
 }
 
-impl<M: Mbc + 'static> Mbc for Boot<M> {
+impl Mbc for Boot {
     fn is_boot(&self) -> bool { true }
-    fn unmap(&mut self) -> Box<dyn Mbc> { Box::new(self.inner.take().unwrap()) }
+    fn unmap(&mut self) -> Box<dyn Mbc> { std::mem::replace(&mut self.inner, Box::new(Unplugged {})) }
 }
 
-impl<MBC: Mbc> MemoryController for Boot<MBC> {
+impl MemoryController for Boot {
     fn new(_: &Rom, _: Vec<u8>) -> Self where Self: Sized {
         unreachable!()
     }
 
-    fn ram_dump(&self) -> Vec<u8> { self.inner.as_ref().unwrap().ram_dump() }
+    fn ram_dump(&self) -> Vec<u8> { self.inner.ram_dump() }
     fn rom_bank(&self) -> usize { 0 }
     fn ram_bank(&self) -> usize { 0 }
 }
 
-impl<MBC: Mbc> Boot<MBC> {
-    pub fn new(rom: &Rom, ram: Vec<u8>, cgb: bool) -> Self {
+impl Boot {
+    pub fn new<MBC: Mbc + 'static>(cgb: bool, mbc: MBC) -> Self {
         Self {
-            boot: if cgb { Box::new(CgbBoot::new())} else { Box::new(DmgBoot::new()) },
-            inner: Some(MBC::new(rom, ram))
+            boot: if cgb { Box::new(CgbBoot::new()) } else { Box::new(DmgBoot::new()) },
+            inner: Box::new(mbc),
         }
     }
 }
